74ee0c31b59473befd9eac74a7e2b4c860d7d781
Mark Ranallo <mranallo@gmail.com>
2011-07-02 18:27:34 -0700

    Initial commit.

---
+++ init.el
@@ -0,0 +1,380 @@
+; enable Common Lisp support
+;(require 'cl)
+
+; some modes need to call stuff on the exec-path
+(push "/usr/local/bin" exec-path)
+
+; Start in server mode
+(server-start)
+
+; add directories to the load path
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/utilities")
+; (add-to-list 'load-path "~/.emacs.d/vendor")
+
+; handy function to load all elisp files in a directory
+(load-file "~/.emacs.d/utilities/utilities.elc")
+
+(add-hook 'emacs-lisp-mode-hook '(lambda ()
+  (add-hook 'after-save-hook 'emacs-lisp-byte-compile t t))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;; ELPA ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("Tromey" . "http://tromey.com/elpa/"))
+(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;; Buffer Tweaks ;;;;;;;;;;;;;;;;;;;;;;;
+
+; recent files
+(require 'recentf)
+(setq recentf-max-saved-items 100)
+
+; disable auto-save files (#foo#)
+(setq auto-save-default nil)
+
+; disable backup files (foo~)
+(setq backup-inhibited t)
+
+; save cursor position within files
+(require 'saveplace)
+(setq save-place-file "~/.emacs.d/.saveplace")
+(setq-default save-place t)
+
+; save minibuffer history across sessions
+(setq savehist-file "~/.emacs.d/.savehist")
+(savehist-mode 1)
+
+; nicer naming of buffers with identical names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator " • ")
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
+
+; Interactively Do Things
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; case insensitive matching
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+
+; automatically clean up old buffers
+(require 'midnight)
+
+; use default Mac browser
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+; delete files by moving them to the OS X trash
+(setq delete-by-moving-to-trash t)
+
+; pick up changes to files on disk automatically (ie, after git pull)
+(global-auto-revert-mode 1)
+
+
+;;;;;;;;;;;;;;;;;;;; Editing Preferences ;;;;;;;;;;;;;;;;;;;;;;;
+
+; tabs
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+; mousing
+(setq mac-emulate-three-button-mouse nil)
+
+; encoding
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+; whitespace
+;(global-whitespace-mode t)
+(setq show-trailing-whitespace t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+; line numbering
+(require 'linum)
+(global-linum-mode)
+(setq linum-format " %d ") ; space after line number
+
+; show column number in bar
+(column-number-mode t)
+; highlight URLs in comments/strings
+(add-hook 'find-file-hooks 'goto-address-prog-mode)
+
+; selection
+(delete-selection-mode t)
+
+; show marks as selections
+(setq transient-mark-mode t)
+
+; highlight matching parens
+(show-paren-mode t)
+
+; highlight incremental search
+(defconst search-highlight t)
+
+; no newlines past EOF
+(setq next-line-add-newlines nil)
+
+; apply syntax highlighting to all buffers
+(global-font-lock-mode t)
+
+;;;;;;;;;;;;;;;;;;;; UI Preferences ;;;;;;;;;;;;;;;;;;;;;;;
+
+(set-frame-font "Menlo-14")
+
+; don't display startup message
+(setq inhibit-startup-message t)
+
+; no scrollbar
+(scroll-bar-mode -1)
+
+; no toolbar
+(tool-bar-mode -1)
+
+; blink cursor
+(blink-cursor-mode t)
+
+; highlight current line
+(global-hl-line-mode t)
+
+; force new frames into existing window
+(setq ns-pop-up-frames nil)
+
+; no bell
+(setq ring-bell-function 'ignore)
+
+; theme
+;(color-theme-twilight)
+;(color-theme-solarized-light)
+;(color-theme-solarized-dark)
+(load-theme 'tango)
+
+;;;;;;;;;;;;;;;;;;;; Keys ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq default-input-method "MacOSX")
+
+; option/alt is meta key
+(setq mac-command-key-is-meta nil)
+
+; Make yes-or-no questions answerable with 'y' or 'n'
+(fset 'yes-or-no-p 'y-or-n-p)
+
+; To be able to M-x without meta - yes, this overwrites exiting but
+; I don't care because I quit Apple style with s-q
+(global-set-key (kbd "C-x C-c") 'execute-extended-command)
+(global-set-key (kbd "C-x c") 'execute-extended-command)
+(global-set-key (kbd "C-x m") 'execute-extended-command)
+
+
+; search with ack
+(global-set-key (kbd "s-F") 'ack)
+
+; open file
+(global-set-key [(super o)] 'find-file)
+
+; buffer switching
+(global-set-key [(super {)] 'previous-buffer)
+(global-set-key [(super })] 'next-buffer)
+
+; window switching
+(global-set-key (kbd "s-`") 'other-window)
+
+; close window
+(global-set-key [(super w)] (lambda ()
+  (interactive)
+  (kill-buffer (current-buffer)
+)))
+
+; navigating through errors
+(global-set-key [(meta n)] 'next-error)
+(global-set-key [(meta p)] 'previous-error)
+
+; run Ruby tests, TextMate-style
+(add-hook 'rinari-minor-mode-hook
+  (lambda ()
+    (define-key rinari-minor-mode-map (kbd "s-r") 'rinari-test)))
+
+; magit
+(global-set-key (kbd "C-c i") 'magit-status)
+
+;; This below is from my previous bindigs file
+
+;; Jump to a definition in the current file. (This is awesome.)
+(global-set-key (kbd "C-x C-i") 'ido-imenu)
+
+;; File finding
+(global-set-key (kbd "C-x M-f") 'ido-find-file-other-window)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(global-set-key (kbd "C-x f") 'ido-find-file)
+(global-set-key (kbd "C-c y") 'bury-buffer)
+(global-set-key (kbd "C-c r") 'revert-buffer)
+(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; If you want to be able to M-x without meta
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+
+;; Duplicate line
+;; (global-set-key (kbd "M-d") 'defunkt-duplicate-line)
+(global-set-key (kbd "M-d") 'duplicate-current-line-or-region)
+
+;; Newline and indent
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; use ibuffer instead of the built in buffer list
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Undo and Redo
+(global-set-key (kbd "s-z") 'undo-tree-undo)
+(global-set-key (kbd "s-Z") 'undo-tree-redo)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; Shell Setup ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; sane path
+(setq path "/Users/mranallo/Code/bin:/opt/local/sbin:/opt/local/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/X11/bin:/Users/mranallo/Code/nodejs/bin")
+(setenv "PATH" path)
+
+; more bash-like autocomplete
+;; (setq eshell-cmpl-cycle-completions nil)
+
+; automatically save history
+;; (setq eshell-save-history-on-exit t)
+
+; ignore version control directories when autocompleting
+;; (setq eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.git\\)/\\'")
+
+; can't write over prompt, that would be weird
+;; (setq comint-prompt-read-only)
+
+; scroll to bottom on output, more like a terminal
+;; (setq eshell-scroll-to-bottom-on-output t)
+;; (setq eshell-scroll-show-maximum-output t)
+
+; colorful shell
+(require 'ansi-color)
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+; escape the shell
+;; (add-hook 'eshell-mode-hook
+  ;; '(lambda nil (local-set-key "\C-u" 'eshell-kill-input)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;; Packages ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  ;; browse-kill-ring   1.3.1        installed  interactively insert items from kill-ring -*- coding: utf-8 -*-
+  ;; color-theme        6.6.1        installed  install color themes
+  ;; color-theme-twi... 0.1          installed  Twilight Colour Theme for Emacs.
+  ;; magit              1.0.0        installed  Control Git from Emacs.
+  ;; magit-simple-keys  1.0.0        installed  simple keybindings for Magit
+  ;; rvm                1.1          installed  Emacs integration for rvm
+  ;; smart-tab          0.3          installed  Intelligent tab completion and indentation.
+  ;; smex               1.1.1        installed  M-x interface with Ido-style fuzzy matching.
+  ;; undo-tree          0.1.6        installed  Treat undo history as a tree
+  ;; yaml-mode          0.0.7        installed  Major mode for editing YAML files
+  ;; yas-jit            0.5          installed  Loads Yasnippets on demand (makes start up faster)
+  ;; yasnippet-bundle   0.6.1        installed  Yet another snippet extension (Auto compiled bundle)
+
+;; Smex
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-x m") 'smex)
+(global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; RVM
+;; (rvm-use-default) ;; use rvm’s default ruby for the current Emacs session
+
+;; textmate mode
+(vendor 'textmate)
+(textmate-mode t)
+
+;; Rinari for rails
+(vendor 'rinari)
+(setq rinari-tags-file-name "TAGS")
+
+;; Haml because they make me
+(vendor 'haml-mode)
+
+;; Minimap like SublimeText2
+(vendor 'minimap)
+
+;; Undo Tree
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+;; Centered Cursor mode
+(vendor 'centered-cursor-mode)
+(global-centered-cursor-mode t)
+
+;; kill ring browsing
+(require 'browse-kill-ring)
+(browse-kill-ring-default-keybindings)
+
+;; dired
+(vendor 'dired+)
+(setq dired-recursive-deletes 'top)
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; sr-speedbar
+(vendor 'sr-speedbar)
+(global-set-key (kbd "s-s") 'sr-speedbar-toggle)
+
+;; RHTML mode
+(add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
+
+;; Ack
+(vendor 'full-ack)
+(autoload 'ack-same "full-ack" nil t)
+(autoload 'ack "full-ack" nil t)
+(autoload 'ack-find-same-file "full-ack" nil t)
+(autoload 'ack-find-file "full-ack" nil t)
+
+;; Ruby
+(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
+(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.gemspec" . ruby-mode) auto-mode-alist))
+
+;; YA Snippets
+(yas/load-directory "~/.emacs.d/vendor/snippets")
+(setq yas/prompt-functions '(yas/ido-prompt
+                             yas/dropdown-prompt
+                             yas/completing-prompt))
+
+(add-hook 'yas/minor-mode-hook
+          (lambda () (define-key yas/minor-mode-map
+                       (kbd "ESC") 'yas/expand))) ; was yas/expand
+(add-hook 'yas/major-mode-hook
+          (lambda () (define-key yas/minor-mode-map
+                       (kbd "ESC") 'yas/expand))) ; was yas/expand
+
+;; smart-tab
+(global-smart-tab-mode 1)
+(setq smart-tab-using-hippie-expand nil)
+
+;; Coffeescript mode
+(vendor 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+          '(lambda() (coffee-custom)))
+
+;; Solarized
+(add-to-list 'load-path "~/.emacs.d/vendor/themes/color-theme-solarized")
+
+;; Ruby End
+(vendor 'ruby-end)
---
+++ utilities/utilities.el
@@ -0,0 +1,140 @@
+(defun chomp (str)
+  "..."
+  (let ((s (if (symbolp str)(symbol-name str) str)))
+    (save-excursion
+      (while (and
+              (not (null (string-match "^\\( \\|\f\\|\t\\|\n\\)" s)))
+              (> (length s) (string-match "^\\( \\|\f\\|\t\\|\n\\)" s)))
+        (setq s (replace-match "" t nil s)))
+      (while (and
+              (not (null (string-match "\\( \\|\f\\|\t\\|\n\\)$" s)))
+              (> (length s) (string-match "\\( \\|\f\\|\t\\|\n\\)$" s)))
+        (setq s (replace-match "" t nil s))))
+    s))
+
+;----------------------------------------------------------------------------
+; Easily count words (http://emacs-fu.blogspot.com/2009/01/counting-words.html)
+;----------------------------------------------------------------------------
+(defun count-words (&optional begin end)
+  "count words between BEGIN and END (region); if no region defined, count words in buffer"
+  (interactive "r")
+  (let ((b (if mark-active begin (point-min)))
+        (e (if mark-active end (point-max))))
+    (message "Word count: %s" (how-many "\\w+" b e))))
+
+
+;; Custom functions not tied to a specific mode go here.
+
+;; Full screen toggle
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+;;  duplicate the current line
+;;  source: http://github.com/defunkt/emacs/
+(defun defunkt-duplicate-line ()
+  (interactive)
+  (beginning-of-line)
+  (copy-region-as-kill (point) (progn (end-of-line) (point)))
+  (textmate-next-line)
+  (yank)
+  (beginning-of-line)
+  (indent-according-to-mode))
+(global-set-key (kbd "M-n") 'defunkt-duplicate-line)
+
+;; Comment code source: http://paste.lisp.org/display/42657
+(defun comment-or-uncomment-line (&optional lines)
+  "Comment current line. Argument gives the number of lines
+			forward to comment"
+  (interactive "P")
+  (comment-or-uncomment-region
+   (line-beginning-position)
+   (line-end-position lines)))
+
+(defun comment-or-uncomment-region-or-line (&optional lines)
+  "If the line or region is not a comment, comments region
+			if mark is active, line otherwise. If the line or region
+			is a comment, uncomment."
+  (interactive "P")
+  (if mark-active
+      (if (< (mark) (point))
+				  (comment-or-uncomment-region (mark) (point))
+				(comment-or-uncomment-region (point) (mark))
+				)
+    (comment-or-uncomment-line lines)))
+
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+			If there's no region, the current line will be duplicated. However, if
+			there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
+(defun delete-this-file ()
+  (interactive)
+  (or (buffer-file-name) (error "no file is currently being edited"))
+  (when (yes-or-no-p "Really delete this file?")
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+      "When called interactively with no active region, copy a single line instead."
+      (interactive
+       (if mark-active (list (region-beginning) (region-end))
+         (message "Copied line")
+         (list (line-beginning-position)
+               (line-beginning-position 2)))))
+    (defadvice kill-region (before slick-cut activate compile)
+      "When called interactively with no active region, kill a single line instead."
+      (interactive
+       (if mark-active (list (region-beginning) (region-end))
+         (list (line-beginning-position)
+               (line-beginning-position 2)))))
+
+; for loading libraries in from the vendor directory
+(defun vendor (library)
+  (let* ((file (symbol-name library))
+         (normal (concat "~/.emacs.d/vendor/" file))
+         (suffix (concat normal ".el")))
+    (cond
+     ((file-directory-p normal)
+      (add-to-list 'load-path normal)
+      (require library))
+     ((file-directory-p suffix)
+      (add-to-list 'load-path suffix)
+      (require library))
+     ((file-exists-p suffix)
+      (require library)))))
