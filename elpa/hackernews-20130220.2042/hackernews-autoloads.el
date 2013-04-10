8bcb7bc25b6513181f38044d1e98f64369ce03ba
Mark Ranallo <mranallo@gmail.com>
2013-04-10 10:21:26 -0700

    Delete stuff, and add themes.

--- elpa/hackernews-20130220.2042/hackernews-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; hackernews-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (hackernews) "hackernews" "hackernews.el" (20836
-;;;;;;  16816 0 0))
-;;; Generated autoloads from hackernews.el
-
-(autoload 'hackernews "hackernews" "\
-The entry point of our client
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("hackernews-pkg.el") (20836 16816 560589
-;;;;;;  0))
-
-;;;***
-
-(provide 'hackernews-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; hackernews-autoloads.el ends here
--- elpa/hackernews-20130220.2042/hackernews-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "hackernews" "20130220.2042" "Access the hackernews aggregator from Emacs [source: github]"
-  '((json "1.2")))
-
